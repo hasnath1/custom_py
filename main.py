e8c9ed934816da2386f65d2f3b138d735bee284a
Hasnath abdullah aknd <72639949+hasnath1@users.noreply.github.com>
2021-01-10 19:59:09 +0600

    Create main.py
    This is the core file that will store all the functions to work with .

---
+++ main.py
@@ -0,0 +1,16 @@
+# Bismillah {BY THE NAME OF ALLAH .}
+
+#func : 1
+# This function converts a string into a list .
+def toList(string):
+    a = string        # For easy-use .
+    i = 0             # Starting point of the string 
+    c_list = []       # This will store the characters in the string
+    while i < len(a):
+        c_list.append(a[i])
+        i += 1
+
+    return c_list     # At last return the value (list) ..
+
+
+
